9ddd91ed643bfae5355970c49c8ed81855f9b3b5
DingYuwen <43669762+DingYuwen@users.noreply.github.com>
2023-01-10 23:17:57 +0800

    chore: config eslint prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,55 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:promise/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'none',
+        singleQuote: true,
+        printWidth: 100,
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  plugins: [
+    'import',
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+    'prettier',
+    '@typescript-eslint',
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
